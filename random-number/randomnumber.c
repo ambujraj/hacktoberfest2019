db15688baf4ebf85ebde6f0814459c8e67d46f28
sunny00786 <56000140+sunny00786@users.noreply.github.com>
2019-10-01 00:50:20 +0530

    Create randomnumber.c
    printing a random number in c

---
+++ random-number/randomnumber.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+  
+
+int main(void) 
+{ 
+    // This program will create same sequence of  
+    // random numbers on every program run  
+ 
+    printf(" %d ", rand()); 
+    return 0; 
+} 
