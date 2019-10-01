16ac204d69e3fc685228412dd20af7dc7d1ef006
Apoorva Gupta <43771033+apoorva-2812@users.noreply.github.com>
2019-10-01 23:28:39 +0530

    Update fact.c

--- factorial/fact.c
+++ factorial/fact.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
-
 void main()
 {
-	int a;
-	scanf("%d", &a);
-	int res = 1;
-	
-	while(a)
-	{res*=a;
-	a--;}
-
-	printf("%d\a", res);
+	int num;
+	scanf("%d", &num);
+	int fact = num;
+	for(int i = 2; i < num; ++i)
+		fact *= i;
+	printf("%d\a", fact);
 }
