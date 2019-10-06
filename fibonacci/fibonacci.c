5d7e05770683fe6b274fb73497261b4c1d3518c3
Dayitva Goel <dayitvagoel@gmail.com>
2019-10-06 19:55:55 +0530

    Updated fibonacci.c to calculate larger terms
    Updated fibonacci.c to calculate larger terms and have a cleaner output

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -1,16 +1,19 @@
-#include<stdio.h>    
-int main()    
-{    
- int n1=0,n2=1,n3,i,number;    
- printf("Enter the number of elements:");    
- scanf("%d",&number);    
- printf("\n%d %d",n1,n2);//printing 0 and 1    
- for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  printf(" %d",n3);    
-  n1=n2;    
-  n2=n3;    
- }  
-  return 0;  
- }    
+#include <stdio.h>
+int main()
+{
+    //This program displays the fibonacci series upto specified counter
+
+    long long int i, n, t1 = 0, t2 = 1, nextTerm;
+    printf("Enter the number of terms: ");
+    scanf("%lld", &n);
+    printf("Fibonacci Series: ");
+    for (i = 1; i <= n; ++i)
+    {
+        printf("%lld, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    printf("\n");
+    return 0;
+}
