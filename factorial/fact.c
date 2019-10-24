3f2c78ec37c5b81a7838452a5f77c0fde486fd70
ADITI YADAV <47411661+AditiYadav06@users.noreply.github.com>
2019-10-24 18:03:11 +0530

    unsing for loop

--- factorial/fact.c
+++ factorial/fact.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
-
-
-void main()
-{
-	int a;
-	scanf("%d", &a);
-	int res = 1;
-	
-	while(a)
-	{res*=a;
-	a--;}
-
-	printf("%d\a", res);
+#include<stdio.h>
+int main(){
+  int i,f=1,num;
+ 
+  printf("Enter a number: ");
+  scanf("%d",&num);
+ 
+  for(i=1;i<=num;i++)
+      f=f*i;
+ 
+  printf("Factorial of %d is: %d",num,f);
+  return 0;
 }
