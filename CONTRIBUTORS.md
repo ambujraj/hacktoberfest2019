c5d38c7eba139ecce257a0c8ed2606f2c7bc6cd6
Alexandre Pereira Santos <alex.endrios@hotmail.com>
2019-10-27 17:06:41 -0300

    Hacktoberfest

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,5 @@
+Name: [Alexandre Santos](Github https://github.com/alexendrios)
+Place: Brasília - DF, Brazil
+About: Quality Assurance Testing Automation
+Programming Language: C, C++, Ruby, Java, Python
+Email: alex.endrios@hotmail.com
---
+++ hello-world/hello_word.rb
@@ -0,0 +1,19 @@
+def greetings
+    message = 'Welcome to hacktoberfest 2019!!!'
+end
+
+def header
+  string = '-----------------------------------------'
+end
+
+def footer
+  string = "2019 DigitalOcean, LLC. All rights reserved."
+end
+
+def presentation
+  p header
+  p greetings
+  p footer
+end
+
+apresentacao
